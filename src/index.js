ae9b1b8f83a7824431c8cf16145f29e9ef30c885
kostanichevski <kostanicevski@gmail.com>
2024-02-07 11:37:16 +0100

    making it flexible

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Bad", "OK", "Good", "Great", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
   </React.StrictMode>
 );
