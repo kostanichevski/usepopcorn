78a16e7c74a45bfd0debdadf1d1de8b242f0cb3d
kostanichevski <kostanicevski@gmail.com>
2024-02-08 12:43:12 +0100

    cleaning up fetch effect

--- src/App.js
+++ src/App.js
@@ -110,12 +110,15 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -124,10 +127,14 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not found");
           setMovies(data.Search);
+          setError("");
           // setIsLoading(false); we added this to the 'finally' block that exectues this after everything is done
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false); // here
         }
@@ -140,6 +147,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   ); //with the empty array braces we specify that this function will only run on moun (the first time the function "APP" is rendered)
